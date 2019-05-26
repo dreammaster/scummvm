bcaa5ae1835a6fbc1b2ee212d31cd689f4d6ab39
Paul Gilbert <dreammaster@scummvm.org>
2019-05-26 12:13:56 -0700

    GLK: TADS3: Updated UTF8 class to handle both const & non-const strings

--- engines/glk/tads/tads3/utf8.cpp
+++ engines/glk/tads/tads3/utf8.cpp
@@ -1,166 +1,144 @@
-#ifdef RCSID
-static char RCSid[] =
-"$Header: d:/cvsroot/tads/tads3/utf8.cpp,v 1.2 1999/05/17 02:52:29 MJRoberts Exp $";
-#endif
-
-/* 
- *   Copyright (c) 1998, 2002 Michael J. Roberts.  All Rights Reserved.
- *   
- *   Please see the accompanying license file, LICENSE.TXT, for information
- *   on using and copying this software.  
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
  */
-/*
-Name
-  utf8.cpp - UTF-8 implementation
-Function
-  
-Notes
-  
-Modified
-  10/17/98 MJRoberts  - Creation
-*/
 
 #include "glk/tads/tads3/utf8.h"
 
-/* ------------------------------------------------------------------------ */
-/*
- *   encode a string of wide characters into the buffer 
- */
-size_t utf8_ptr::setwchars(const wchar_t *src, size_t src_count,
-                           size_t bufsiz)
-{
-    /* loop through the source and store the characters */
-    size_t outbytes;
-    for (outbytes = 0 ; src_count > 0 ; --src_count, ++src)
-    {
-        /* figure out how many bytes we need for this character */
-        size_t curbytes = s_wchar_size(*src);
+namespace Glk {
+namespace TADS {
+namespace TADS3 {
 
-        /* add it to the total output size */
-        outbytes += curbytes;
+size_t utf8_ptr::setwchars(const wchar_t *src, size_t src_count, size_t bufsiz) {
+	// Loop through the source and store the characters
+	size_t outbytes;
+	for (outbytes = 0 ; src_count > 0 ; --src_count, ++src) {
+		// figure out how many bytes we need for this character
+		size_t curbytes = s_wchar_size(*src);
 
-        /* if we have room, add it to the buffer */
-        if (bufsiz >= curbytes)
-        {
-            /* store it */
-            setch(*src);
+		// add it to the total output size
+		outbytes += curbytes;
+
+		// if we have room, add it to the buffer
+		if (bufsiz >= curbytes)
+		{
+			// store it
+			setch(*src);
             
-            /* deduct this space from the remaining buffer size */
-            bufsiz -= curbytes;
-        }
-        else
-        {
-            /* 
-             *   there's no room for this - make sure we don't store
-             *   anything more (since we might have room for a shorter
-             *   character later, but that would put a gap in the output
-             *   string - better to just truncate here) 
-             */
-            bufsiz = 0;
-        }
-    }
+			// deduct this space from the remaining buffer size
+			bufsiz -= curbytes;
+		} else {
+			/*
+			 * There's no room for this - make sure we don't store
+			 * anything more (since we might have room for a shorter
+			 * character later, but that would put a gap in the output
+			 * string - better to just truncate here) 
+			 */
+			bufsiz = 0;
+		}
+	}
 
-    /* return the total output size used (or needed) */
-    return outbytes;
+	// return the total output size used (or needed)
+	return outbytes;
 }
 
-/* ------------------------------------------------------------------------ */
-/*
- *   encode a null-terminated string of wide characters into the buffer 
- */
-size_t utf8_ptr::setwcharsz(const wchar_t *src, size_t bufsiz)
-{
-    /* loop through the source and store the characters */
-    size_t outbytes;
-    for (outbytes = 0 ; *src != 0 ; ++src)
-    {
-        /* figure out how many bytes we need for this character */
-        size_t curbytes = s_wchar_size(*src);
+size_t utf8_ptr::setwcharsz(const wchar_t *src, size_t bufsiz) {
+	// loop through the source and store the characters
+	size_t outbytes;
+	for (outbytes = 0 ; *src != 0 ; ++src) {
+		// figure out how many bytes we need for this character
+		size_t curbytes = s_wchar_size(*src);
 
-        /* add it to the total output size */
-        outbytes += curbytes;
+		// add it to the total output size
+		outbytes += curbytes;
 
-        /* if we have room, add it to the buffer */
-        if (bufsiz >= curbytes)
-        {
-            /* store it */
-            setch(*src);
+		// if we have room, add it to the buffer
+		if (bufsiz >= curbytes) {
+			// store it
+			setch(*src);
 
-            /* deduct this space from the remaining buffer size */
-            bufsiz -= outbytes;
-        }
-        else
-        {
-            /* 
-             *   there's no room for this - make sure we don't store
-             *   anything more (since we might have room for a shorter
-             *   character later, but that would put a gap in the output
-             *   string - better to just truncate here) 
-             */
-            bufsiz = 0;
-        }
-    }
+			// deduct this space from the remaining buffer size
+			bufsiz -= outbytes;
+		} else {
+			/*
+			 * There's no room for this - make sure we don't store
+			 * anything more (since we might have room for a shorter
+			 * character later, but that would put a gap in the output
+			 * string - better to just truncate here) 
+			 */
+			bufsiz = 0;
+		}
+	}
 
-    /* return the total output size used (or needed) */
-    return outbytes;
+	// return the total output size used (or needed)
+	return outbytes;
 }
 
-/* ------------------------------------------------------------------------ */
-/*
- *   Compare this string to the given string 
- */
 int utf8_ptr::s_compare_to(const char *p1, size_t bytelen1,
-                           const char *p2, size_t bytelen2)
-{
-    /* keep going until one or the other string runs out of bytes */
-    while (bytelen1 != 0 && bytelen2 != 0)
-    {
-        wchar_t c1, c2;
-        size_t siz1, siz2;
+                           const char *p2, size_t bytelen2) {
+	// keep going until one or the other string runs out of bytes
+	while (bytelen1 != 0 && bytelen2 != 0) {
+		wchar_t c1, c2;
+		size_t siz1, siz2;
         
-        /* get the current character from each string */
-        c1 = s_getch(p1);
-        c2 = s_getch(p2);
+		// get the current character from each string
+		c1 = s_getch(p1);
+		c2 = s_getch(p2);
 
-        /* compare them */
-        if (c1 > c2)
-            return 1;
-        else if (c1 < c2)
-            return -1;
+		// compare them
+		if (c1 > c2)
+			return 1;
+		else if (c1 < c2)
+			return -1;
 
-        /* get the size of each character */
-        siz1 = s_charsize(*p1);
-        siz2 = s_charsize(*p2);
+		// get the size of each character
+		siz1 = s_charsize(*p1);
+		siz2 = s_charsize(*p2);
 
-        /* decrement each counter by the byte size of this character */
-        bytelen1 -= siz1;
-        bytelen2 -= siz2;
+		// decrement each counter by the byte size of this character
+		bytelen1 -= siz1;
+		bytelen2 -= siz2;
 
-        /* advance to the next character in each string */
-        p1 += siz1;
-        p2 += siz2;
-    }
+		// advance to the next character in each string
+		p1 += siz1;
+		p2 += siz2;
+	}
 
-    /* 
-     *   we didn't find any character differences, but one string is
-     *   longer than the other -- if they ran out at the same time,
-     *   they're identical; otherwise, the one that ran out first is the
-     *   lesser one 
-     */
-    if (bytelen2 != 0)
-    {
-        /* the first one ran out first, so the first one sorts earlier */
-        return -1;
-    }
-    else if (bytelen1 != 0)
-    {
-        /* the second one ran out first, so the first one sort later */
-        return 1;
-    }
-    else
-    {
-        /* they both ran out at the same time */
-        return 0;
-    }
+	/* 
+	 * we didn't find any character differences, but one string is
+	 * longer than the other -- if they ran out at the same time,
+	 * they're identical; otherwise, the one that ran out first is the
+	 * lesser one 
+	 */
+	if (bytelen2 != 0) {
+		// the first one ran out first, so the first one sorts earlier
+		return -1;
+	} else if (bytelen1 != 0) {
+		// the second one ran out first, so the first one sort later
+		return 1;
+	} else {
+		// they both ran out at the same time
+		return 0;
+	}
 }
 
+} // End of namespace TADS2
+} // End of namespace TADS
+} // End of namespace Glk
