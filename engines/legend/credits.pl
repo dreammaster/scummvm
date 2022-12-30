6d801c42f9aec1108c56ed6704360e5d9eac38da
Paul Gilbert <dreammaster@scummvm.org>
2022-12-29 14:33:45 -1000

    LEGEND: Skeleton engine

---
+++ engines/legend/credits.pl
@@ -0,0 +1,3 @@
+begin_section("Legend");
+	add_person("Name 1", "Handle 1", "");
+end_section();
---
+++ engines/legend/detection.cpp
@@ -0,0 +1,45 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "base/plugins.h"
+#include "common/config-manager.h"
+#include "common/file.h"
+#include "common/md5.h"
+#include "common/str-array.h"
+#include "common/translation.h"
+#include "common/util.h"
+#include "legend/detection.h"
+#include "legend/detection_tables.h"
+
+const DebugChannelDef LegendMetaEngineDetection::debugFlagList[] = {
+	{ Legend::kDebugGraphics, "Graphics", "Graphics debug level" },
+	{ Legend::kDebugPath, "Path", "Pathfinding debug level" },
+	{ Legend::kDebugFilePath, "FilePath", "File path debug level" },
+	{ Legend::kDebugScan, "Scan", "Scan for unrecognised games" },
+	{ Legend::kDebugScript, "Script", "Enable debug script dump" },
+	DEBUG_CHANNEL_END
+};
+
+LegendMetaEngineDetection::LegendMetaEngineDetection() : AdvancedMetaEngineDetection(Legend::gameDescriptions,
+	sizeof(ADGameDescription), Legend::legendGames) {
+}
+
+REGISTER_PLUGIN_STATIC(LEGEND_DETECTION, PLUGIN_TYPE_ENGINE_DETECTION, LegendMetaEngineDetection);
