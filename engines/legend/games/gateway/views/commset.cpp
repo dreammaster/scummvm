f0dfa5f6680c705fffcc77905e89b2796a992f7a
Paul Gilbert <dreammaster@scummvm.org>
2023-01-01 16:25:22 -1000

    LEGEND: Starting implementation of CommSet view

--- engines/legend/games/gateway/views/commset.cpp
+++ engines/legend/games/gateway/views/commset.cpp
@@ -26,8 +26,43 @@ namespace Early {
 namespace Gateway {
 namespace Views {
 
+enum {
+	BTN_NONE, BTN_1, BTN_2, BTN_3, BTN_4, BTN_5, BTN_6,
+	BTN_7, BTN_8, BTN_9, BTN_PREV, BTN_0, BTN_NEXT
+};
+
+CommSetContents::CommSetContents(UIElement *parent, const Common::Rect &area) :
+	Legend::Early::Gfx::View(parent, area) {
+}
+
+void CommSetContents::draw() {
+	// TODO
+}
+
+CommSet::CommSet() : Legend::Early::Gfx::View("CommSet"), _logo(nullptr),
+		_btn1(this, "1", BTN_1, Common::Rect(44, 113, 84, 135)),
+		_btn2(this, "2", BTN_2, Common::Rect(88, 113, 128, 135)),
+		_btn3(this, "3", BTN_3, Common::Rect(132, 113, 172, 135)),
+		_btn4(this, "4", BTN_4, Common::Rect(44, 139, 84, 161)),
+		_btn5(this, "5", BTN_5, Common::Rect(88, 139, 128, 161)),
+		_btn6(this, "6", BTN_6, Common::Rect(132, 139, 172, 161)),
+		_btn7(this, "7", BTN_7, Common::Rect(44, 165, 84, 187)),
+		_btn8(this, "8", BTN_8, Common::Rect(88, 165, 128, 187)),
+		_btn9(this, "9", BTN_9, Common::Rect(132, 165, 172, 187)),
+		_btnPrev(this, "Prev", BTN_PREV, Common::Rect(44, 191, 84, 213)),
+		_btn0(this, "0", BTN_0, Common::Rect(88, 191, 128, 213)),
+		_btnNext(this, "Next", BTN_NEXT, Common::Rect(132, 191, 172, 213)),
+		_contents(this, Common::Rect(188, 56, 596, 424)) {
+	setFont(2);
+}
+
+CommSet::~CommSet() {
+	delete _logo;
+}
+
 void CommSet::draw() {
 	// TODO
+
 }
 
 } // namespace Views
