751526de5c634c78b3b8972ba4a36757abc4de13
Paul Gilbert <dreammaster@scummvm.org>
2023-01-11 16:16:13 -1000

    LEGEND: Added string console command

--- engines/legend/console.h
+++ engines/legend/console.h
@@ -29,7 +29,7 @@ namespace Legend {
 
 class Console : public GUI::Debugger {
 private:
-	bool Cmd_test(int argc, const char **argv);
+	bool cmdString(int argc, const char **argv);
 public:
 	Console();
 	~Console() override;
