d56e9a142f1d9ffa89fa27ed020d8561e78bbcf0
Paul Gilbert <dreammaster@scummvm.org>
2024-06-09 10:47:35 -0700

    WASTELAND: Adding button class

--- engines/wasteland/module.mk
+++ engines/wasteland/module.mk
@@ -17,6 +17,7 @@ MODULE_OBJS = \
 	gfx/tiles.o \
 	gfx/wasteland_font.o \
 	wasteland1/wasteland1.o \
+	wasteland1/gfx/button.o \
 	wasteland1/gfx/cursors.o \
 	wasteland1/gfx/color_font.o \
 	wasteland1/gfx/mono_font.o \
