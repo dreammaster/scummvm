7820f2a712e9438655f4ede1f06d27ff5497bdf7
Paul Gilbert <dreammaster@scummvm.org>
2024-07-04 09:45:20 -0700

    WASTELAND: Some Huffman fixes

--- engines/wasteland/wasteland1/files/huffman_tree.cpp
+++ engines/wasteland/wasteland1/files/huffman_tree.cpp
@@ -27,15 +27,15 @@ namespace Wasteland {
 namespace Wasteland1 {
 namespace Huffman {
 
-HuffmanTree *HuffmanTree::create(Common::ReadStream *src) {
+HuffmanTree *HuffmanTree::load(Common::ReadStream *src) {
 	HuffmanTree *tree = new HuffmanTree();
 	BitStream *bitStream = dynamic_cast<BitStream *>(src);
 
 	if (bitStream) {
-		tree->createTree(bitStream);
+		tree->loadTree(bitStream);
 	} else {
 		BitStream streamWrapper(src);
-		tree->createTree(&streamWrapper);
+		tree->loadTree(&streamWrapper);
 	}
 
 	return tree;
@@ -46,49 +46,28 @@ HuffmanTree::~HuffmanTree() {
 		delete it->_value;
 }
 
-HuffmanTree *HuffmanTree::create(const Common::Array<byte> &bytes) {
-	Common::MemoryReadStream stream(&bytes[0], bytes.size());
-	return create(&stream);
+void HuffmanTree::loadTree(BitStream *stream) {
+	_rootNode = loadNode(stream);
 }
 
-void HuffmanTree::createTree(BitStream *stream) {
-	int *counter;
+HuffmanNode *HuffmanTree::loadNode(BitStream *stream) {
 	int b;
-	HuffmanNode *node, *left, *right;
-	Common::SortedArray<HuffmanNode *, const HuffmanNode *> nodes(HuffmanNode::compareNodes);
+	HuffmanNode *node;
 
-	// Read the stream and build the counter array
-	counter = new int[256];
-	while ((b = stream->readByte()) != -1)
-	{
-		counter[b]++;
+	if (stream->readBit() == 0) {
+		HuffmanNode *left, *right;
+
+		left = loadNode(stream);
+		stream->readBit();
+		right = loadNode(stream);
+		return new HuffmanNode(left, right);
+
+	} else {
+		b = stream->readByte();
+		node = new HuffmanNode(b);
+		this->_index[b] = node;
+		return node;
 	}
-
-	// Initialize the nodes with it's payloads and probabilities
-	for (int i = 0; i < 256; i++) {
-		if (counter[i] == 0)
-			continue;
-
-		node = new HuffmanNode(i, counter[i]);
-		nodes.insert(node);
-		_index[i] = node;
-	}
-
-	// Repeat until only one node (the root node) is left
-	while (nodes.size() > 1) {
-		// Take the last two nodes from the list
-		left = nodes.back();
-		nodes.remove(left);
-		right = nodes.back();
-		nodes.remove(right);
-
-		// Create the parent node for them and add it to the list
-		node = new HuffmanNode(left, right);
-		nodes.insert(node);
-	}
-
-	// Save the root node
-	_rootNode = nodes.front();
 }
 
 } // namespace Huffman
