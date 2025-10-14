bc243c402d7ca2f2a0d270d29afaa320ae80cd3f
Paul Gilbert <dreammaster@scummvm.org>
2025-10-14 01:16:10 -0700

    SPYCRAFT: Further stubs in duck_api.cpp

--- engines/spycraft/dmade/duck_api.cpp
+++ engines/spycraft/dmade/duck_api.cpp
@@ -24,6 +24,115 @@
 
 namespace Spycraft {
 
+void DumpDelta() {
+	warning("TODO: duck_api");
+}
+
+void BlackBox(unsigned char *addr, int x, int y, int w, int h, int pitch) {
+	warning("TODO: duck_api");
+}
+
+void BlackDuck(DXL_VSCREEN_HANDLE vsp) {
+	warning("TODO: duck_api");
+}
+
+void Announcement(const char *lpszString) {
+	warning("TODO: duck_api");
+}
+
+void AnnDone() {
+	warning("TODO: duck_api");
+}
+
+void longjmpOnAudioErrors(jmp_buf *pErrorContext) {
+	warning("TODO: duck_api");
+}
+void ErrorBuffer(char *errorbuf) {
+	warning("TODO: duck_api");
+}
+int RestoreDirty(DXL_XIMAGE_HANDLE pXImageint, DXL_VSCREEN_HANDLE dst) {
+	warning("TODO: duck_api");
+	return 0;
+}
+int doVGASetup(DXL_VSCREEN_HANDLE dst, char *bgstring) {
+	warning("TODO: duck_api");
+	return 0;
+}
+void resetDisplay(DXL_VSCREEN_HANDLE dst) {
+	warning("TODO: duck_api");
+}
+void flip_screens(DXL_VSCREEN_HANDLE dst) {
+	warning("TODO: duck_api");
+}
+void MarkSkips(DXL_VSCREEN_HANDLE dst, int skipnum) {
+	warning("TODO: duck_api");
+}
+int GetAudioProfile() {
+	warning("TODO: duck_api");
+	return 0;
+}
+int Get_show_dots() {
+	warning("TODO: duck_api");
+	return 0;
+}
+void Set_show_dots(int fred) {
+	warning("TODO: duck_api");
+}
+int DLLVersion() {
+	warning("TODO: duck_api");
+	return 0;
+}
+int Get_ZBuffer() {
+	warning("TODO: duck_api");
+	return 0;
+}
+void NewTrigger(int val) {
+	warning("TODO: duck_api");
+}
+int FillAudio(HFB_BUFFER_HANDLE HFBuffer, MFP_STREAM_HANDLE AStream, int *pAudioChunkIndex,
+	void **audaddr, long *audlen, int buffPreload, int MultiBlock) {
+	warning("TODO: duck_api");
+	return 0;
+}
+int SetupAudio(HFB_BUFFER_HANDLE HFBuffer, MFP_STREAM_HANDLE AStream, int *pAudioChunkIndex,
+	void **audaddr, long *audlen, int buffPreload) {
+	warning("TODO: duck_api");
+	return 0;
+}
+void StartPlaying() {
+	warning("TODO: duck_api");
+}
+
+void StopPlaying() {
+	warning("TODO: duck_api");
+}
+
+void EndAudio() {
+	warning("TODO: duck_api");
+}
+
+void TakeBack() {
+	warning("TODO: duck_api");
+}
+
+void PauseSpeaker() {
+	warning("TODO: duck_api");
+}
+
+void ResumeSpeaker() {
+	warning("TODO: duck_api");
+}
+
+int TimeSpeaker() {
+	warning("TODO: duck_api");
+	return 0;
+}
+
+int IsPlaying() {
+	warning("TODO: duck_api");
+	return 0;
+}
+
 void SetStates(spkrst8 speaker, int S3style) {
 	warning("TODO: duck_api SetStates");
 }
