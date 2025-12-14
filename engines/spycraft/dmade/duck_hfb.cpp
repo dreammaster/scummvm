f14279e196d5101d6f6b8f37d6c7cc3794a27ba5
Paul Gilbert <dreammaster@scummvm.org>
2025-12-14 14:21:39 +1100

    SPYCRAFT: Clarify duck_hfb TODOs

--- engines/spycraft/dmade/duck_hfb.cpp
+++ engines/spycraft/dmade/duck_hfb.cpp
@@ -32,135 +32,135 @@ void MFP_Exit(void) {
 }
 
 MFP_FILE_HANDLE MFP_OpenFile(const char *FileName, HFB_BUFFER_HANDLE bfHnd) {
-	error("TODO: duck_hfb");
+	error("TODO: MFP_OpenFile");
 }
 
 MFP_FILE_HANDLE MFP_FindFile(const char *FileName) {
-	error("TODO: duck_hfb");
+	error("TODO: MFP_FindFile");
 }
 
 int MFP_ParseFile(MFP_FILE_HANDLE fHnd, HFB_BUFFER_HANDLE bfHnd) {
-	error("TODO: duck_hfb");
+	error("TODO: MFP_ParseFile");
 }
 
 void MFP_LoadIndex(MFP_FILE_HANDLE fHnd, HFB_BUFFER_HANDLE bfHnd) {
-	error("TODO: duck_hfb");
+	error("TODO: MFP_LoadIndex");
 }
 
 void MFP_CloseFile(MFP_FILE_HANDLE FileHandle) {
-	error("TODO: duck_hfb");
+	error("TODO: MFP_CloseFile");
 }
 
 MFP_FILE_INFO *MFP_GetFileInfo(MFP_FILE_HANDLE FileHandle) {
-	error("TODO: duck_hfb");
+	error("TODO: MFP_GetFileInfo");
 }
 
 MFP_STREAM_HANDLE MFP_GetStream(MFP_FILE_HANDLE FileHandle, const char *StreamName, int StreamNum, unsigned long SType) {
-	error("TODO: duck_hfb");
+	error("TODO: MFP_GetStream");
 }
 
 void MFP_ReleaseStream(MFP_STREAM_HANDLE StreamHandle) {
-	error("TODO: duck_hfb");
+	error("TODO: MFP_ReleaseStream");
 }
 
 MFP_STREAM_INFO *MFP_GetStreamInfo(MFP_STREAM_HANDLE StreamHandle) {
-	error("TODO: duck_hfb");
+	error("TODO: MFP_GetStreamInfo");
 }
 
 int MFP_ReadData(MFP_STREAM_HANDLE StreamHandle, void *Data, long *MaxLength, enum dukDirect Direction, int Count) {
-	error("TODO: duck_hfb");
+	error("TODO: MFP_ReadData");
 }
 
 DKWAVEFORM *MFP_GetAudioInfo(MFP_STREAM_HANDLE aStream, int *lNumChannels, int *lSamplesPerSec, int *lBytesPerSec, int *lwFormat) {
-	error("TODO: duck_hfb");
+	error("TODO: MFP_GetAudioInfo");
 }
 
 int MFP_GetSamplesPerFrame(MFP_STREAM_HANDLE vStream, MFP_STREAM_HANDLE aStream) {
-	error("TODO: duck_hfb");
+	error("TODO: MFP_GetSamplesPerFrame");
 }
 
 void MFP_GetFrameRates(MFP_STREAM_HANDLE vStream, MFP_STREAM_HANDLE aStream, int *lGlobalFrameRate, int *prebuffer) {
-	error("TODO: duck_hfb");
+	error("TODO: MFP_GetFrameRates");
 }
 
 HFB_BUFFER_HANDLE HFB_CreateBuffer(long bSize, long minIndexFrames) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_CreateBuffer");
 }
 
 int HFB_ConditionBuffer(HFB_BUFFER_HANDLE Buffer, long bSize, long minIndexFrames) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_ConditionBuffer");
 }
 
 void HFB_InitBuffer(HFB_BUFFER_HANDLE Buffer, MFP_FILE_HANDLE File, int startFrame, int preload) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_InitBuffer");
 }
 
 void HFB_DestroyBuffer(HFB_BUFFER_HANDLE Buffer) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_DestroyBuffer");
 }
 
 int HFB_SetBufferMode(HFB_BUFFER_HANDLE Buffer, enum HFB_Modes newMode) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_SetBufferMode");
 }
 
 MFP_FILE_HANDLE HFB_QueueOpenFile(const char *FileName, HFB_BUFFER_HANDLE bfHnd) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_QueueOpenFile");
 }
 
 long HFB_getDataPosition(MFP_STREAM_HANDLE dckPtr, HFB_DATA_HANDLE chunkIndex) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_getDataPosition");
 }
 
 long HFB_GetBufferPerCentFull(HFB_BUFFER_HANDLE dckPtr) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_GetBufferPerCentFull");
 }
 
 int HFB_FillBuffer(HFB_BUFFER_HANDLE bfHnd, long AmountToFill, long ElapsedFrames) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_FillBuffer");
 }
 
 int HFB_getBufferStatus(HFB_BUFFER_HANDLE dckPtr) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_getBufferStatus");
 }
 
 HFB_DATA_HANDLE HFB_GetStreamingData(MFP_STREAM_HANDLE StreamHandle, void **DataPointer, long *Length, enum dukDirect Direction, int Count) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_GetStreamingData");
 }
 
 void HFB_ReleaseStreamingData(HFB_BUFFER_HANDLE bfHnd, HFB_DATA_HANDLE DataHandle) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_ReleaseStreamingData");
 }
 
 int HFB_GetIndexFlags(MFP_STREAM_HANDLE sPtr, enum FTYPE format, int frameNum) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_GetIndexFlags");
 }
 
 int HFB_FramestoNextKeyFrame(MFP_STREAM_HANDLE sPtr, int ChunkIndex, int *pChunkINdex) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_FramestoNextKeyFrame");
 }
 
 int HFB_FrameToChunk(MFP_STREAM_HANDLE sPtr, int FrameNum) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_FrameToChunk");
 }
 
 int HFB_PreviousKeyFrame(MFP_STREAM_HANDLE sPtr, int FrameNum) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_PreviousKeyFrame");
 }
 
 int HFB_PlayFile(const char *FileName, long bSize, int PreLoad) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_PlayFile");
 }
 
 void HFB_WalkFlags(HFB_BUFFER_HANDLE bfHnd) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_WalkFlags");
 }
 
 long HFB_GetBufferSpace(HFB_BUFFER_HANDLE dckPtr) {
-	error("TODO: duck_hfb");
+	error("TODO: HFB_GetBufferSpace");
 }
 
 int MFP_GetInitialFrames(MFP_STREAM_HANDLE vStream, MFP_STREAM_HANDLE aStream) {
-	error("TODO: duck_hfb");
+	error("TODO: MFP_GetInitialFrames");
 }
 
 } // namespace Spycraft
